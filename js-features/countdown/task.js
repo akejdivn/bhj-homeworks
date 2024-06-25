d759a2053b246ec1852cc58d92bab1c960193384
Alena <murawievaa@yandex.ru>
2024-06-25 20:38:38 +0300

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,20 @@
+const timer = document.getElementById('timer');
+let time = 5;
+
+
+
+
+let interval = setInterval(countdown, 1000)
+
+
+function countdown() {
+    
+    if(time === 0){
+        alert('Вы победили в конкурсе! Теперь Вы котенок!');
+        clearInterval(interval)
+    } else {
+        timer.innerHTML = `${time}`;
+        time--;
+    }
+    
+}
