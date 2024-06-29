5607448d2b9a1fc69fd9a708eebc57d30d8573d7
Alena <murawievaa@yandex.ru>
2024-06-29 16:19:24 +0300

    задание выполено

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,12 +1,13 @@
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost")
-const hole = document.getElementsByClassName("hole")
+const hole = Array.from(document.getElementsByClassName("hole"))
 
 let counterDead = 0;
 let counterLost = 0;
 
-hole.onclick = function(){
-    if(hole.className.includes( 'hole_has-mole' )){
+hole.forEach((item) => {
+item.onclick = function(){
+    if(item.className.includes('hole_has-mole')){
         counterDead++;
         dead.textContent = counterDead
     }
@@ -14,13 +15,16 @@ hole.onclick = function(){
         counterLost++;
         lost.textContent = counterLost
     }
-} 
-
-if(dead === 10){
-    alert ("Победа!");
-   lost = 0;
-   dead = 0;
-} else if(lost === 5){
-   lost = 0;
-   dead = 0
+    if(counterDead === 10){
+        alert ("Победа!");
+        document.location.reload();
+    } else if(counterLost === 5){
+        alert ("Вы проиграли!");
+        document.location.reload();
+    }
+    
 }
+}
+)
+    
+
