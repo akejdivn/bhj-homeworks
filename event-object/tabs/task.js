6ecb27ec02b5eecdbc408f574f0b7b079a9e40d3
Alena <murawievaa@yandex.ru>
2024-08-11 17:40:43 +0300

    выполненное задание

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,11 +1,31 @@
-const tab = document.querySelectorAll('tab')
-const tabContent = document.querySelectorAll('tab__content');
+const tab = Array.from(document.querySelectorAll('.tab'))
+const tabContent = Array.from(document.querySelectorAll('.tab__content'));
+const tabNavigation = document.getElementsByClassName('tab__navigation')
 
-tab.onclick = function() {
-if(!tab.classList.contains('tab_active')){
-    tab.classList.add('tab_active');
-   
-} else {
-    tab.classList.remove('tab_active');
+for(let i = 0; i< tab.length; i++){
+    for(let i = 0; i< tabContent.length; i++){
+    tab[i].addEventListener('click', function(){
+
+        let tabActive = tab.findIndex(tab =>{
+            if(tab.classList.contains('tab_active')){
+                return true
+            }
+        })
+        let tabContentActive = tabContent.findIndex(tabContent =>{
+            if(tabContent.classList.contains('tab__content_active')){
+                return true
+            }
+        })
+        
+        tab[tabActive].classList.remove('tab_active');
+        tab[i].classList.add('tab_active')
+       
+        tabContent[tabContentActive].classList.remove('tab__content_active');
+        tabContent[i].classList.add('tab__content_active')
+        
+    })
 }
 }
+
+
+ 
