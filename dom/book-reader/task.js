33224c14dd2e2182e543f5bb826fb611e65df1d0
Alena <murawievaa@yandex.ru>
2024-08-20 19:20:08 +0300

    выполненное задание

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -2,35 +2,34 @@ const book = document.querySelector('.book');
 const size = Array.from(document.querySelectorAll('.font-size'));
 
 size.forEach(el =>{
-    el.onclick = function(){
+            el.onclick = function(){
+                size.forEach(el =>{
 
-        if(el.classList.contains('font-size_small')){
-           el.classList.add('font-size-active')
-            book.classList.add('book_fs-small')
-            book.classList.remove('book_fs-big')
-            if(el.classList.contains('font-size_big')){
-                el.classList.remove('font-size-active')
-            }
-           
-        }  else if(el.classList.contains('font-size_big')){
-            el.classList.add('font-size-active')
-            book.classList.add('book_fs-big')
-            book.classList.remove('book_fs-small')
-            if(el.classList.contains('font-size_small')){
-                el.classList.remove('font-size-active')
-            }
-           
-        } 
-        else{
-            book.classList.remove('book_fs-small') 
-            book.classList.remove('book_fs-big')
-             
+                el.classList.remove('font-size_active')
+                this.classList.add('font-size_active')
+                book.classList.remove('book_fs-small')
+                book.classList.remove('book_fs-big')
+
+        if(this.classList.contains('font-size_small')){
+             book.classList.add('book_fs-small')
         }
-        return false
+               else if(this.classList.contains('font-size_big')){
+             book.classList.add('book_fs-big')
+                 }
+         else {
+             book.classList.remove('book_fs-small') 
+             book.classList.remove('book_fs-big')
+         }
+         }
+                )
+                return false
+            }
         
-    }
-}
-)
+           
+        }
+    )
+    
+     
     
 
    
