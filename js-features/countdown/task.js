466c488aff906f3f177f513ea7671647eb9b0529
Alena <murawievaa@yandex.ru>
2024-11-05 20:43:40 +0300

    first commit countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,16 +1,11 @@
 const timer = document.getElementById('timer');
 let time = 59;
 
-
-
-
 let interval = setInterval(countdown, 1000)
 
-
 function countdown() {
-    
     if(time === 0){
-        alert('Вы победили в конкурсе! Теперь Вы котенок!');
+        alert('Вы победили в конкурсе! ');
         clearInterval(interval)
     } else {
         timer.innerHTML = `${time}`;
